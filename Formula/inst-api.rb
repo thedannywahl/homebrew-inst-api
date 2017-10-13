c1ccb5487bd4a02f6cf7f98a80c80ffcd2118157
Danny Wahl <danny@iyware.com>
2017-10-13 14:25:06 -0600

    v0.3.0

--- Formula/inst-api.rb
+++ Formula/inst-api.rb
@@ -1,8 +1,8 @@
 class InstApi < Formula
   desc "A collection of wrappers for Instructure APIs"
   homepage "https://github.com/thedannywahl/inst-api"
-  url "https://github.com/thedannywahl/inst-api/archive/v0.2.3.zip"
-  sha256 "2932208bad3e37c528d438155f20dbd8f6990bed81d1a504c3759017b5fcc3b5"
+  url "https://github.com/thedannywahl/inst-api/archive/v0.3.0.zip"
+  sha256 "8c0436825dca9267a47b70561093757a0dce032f88c4a30d034a1ce1d8342db9"
 
   def install
     bin.install Dir["arc/arc"], Dir["bridge/bridge"], Dir["canvas/canvas"], Dir["catalog/catalog"], Dir["commons/commons"]
