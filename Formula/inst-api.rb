b5fdeb0a855bc244bde272ea86d1112a679942cc
Danny Wahl <danny@iywahl.com>
2019-08-02 11:12:57 -0600

    add jq dependency

--- Formula/inst-api.rb
+++ Formula/inst-api.rb
@@ -4,6 +4,8 @@ class InstApi < Formula
   url "https://github.com/thedannywahl/inst-api/archive/v1.0.0.zip"
   sha256 "da14e7c128c1f6a2f7088c00ef86ac8d910fbb31fc14cc602bbf193715aa93b9"
 
+  depends_on "jq"
+
   def install
     bin.install Dir["studio/studio"], Dir["bridge/bridge"], Dir["canvas/canvas"], Dir["catalog/catalog"], Dir["cdata/cdata"], Dir["commons/commons"], Dir["insthelp/insthelp"]
   end
@@ -21,7 +23,7 @@ class InstApi < Formula
     system "#{bin}/catalog -v"
     system "#{bin}/commons -v"
     system "#{bin}/cdata -v"
-    system "#{bin}/insthelp version insthelp"
+    system "#{bin}/insthelp version insthelp 1.0.0"
   end
 
 end
