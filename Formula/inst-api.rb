155c1323281e75640b1a9da530b5c494774751c5
Danny Wahl <danny@iywahl.com>
2019-10-01 12:23:21 -0600

    v1.0.1

--- Formula/inst-api.rb
+++ Formula/inst-api.rb
@@ -1,8 +1,8 @@
 class InstApi < Formula
   desc "A collection of wrappers for Instructure APIs"
   homepage "https://github.com/thedannywahl/inst-api"
-  url "https://github.com/thedannywahl/inst-api/archive/v1.0.0.zip"
-  sha256 "da14e7c128c1f6a2f7088c00ef86ac8d910fbb31fc14cc602bbf193715aa93b9"
+  url "https://github.com/thedannywahl/inst-api/archive/v1.0.1.zip"
+  sha256 "43d8a325559e72d3e9eb9fb5970780a95e84bf2fab2e90c1a0238efa7efbb0d4"
 
   depends_on "jq"
 
