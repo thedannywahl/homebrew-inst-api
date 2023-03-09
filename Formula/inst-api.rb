6c478d93763bba684403b4ea6a8759948dc96dfe
Danny Wahl <danny@iywahl.com>
2023-03-09 16:07:10 -0700

    v1.1.2

--- Formula/inst-api.rb
+++ Formula/inst-api.rb
@@ -1,24 +1,23 @@
 class InstApi < Formula
   desc "A collection of wrappers for Instructure APIs"
   homepage "https://github.com/thedannywahl/inst-api"
-  url "https://github.com/thedannywahl/inst-api/archive/v1.1.1.zip"
-  sha256 "0c4363ba968ffdfae7b71187189ee0dee1677f59bf631a7141f0021b22bdef64"
+  url "https://github.com/thedannywahl/inst-api/archive/v1.1.2.zip"
+  sha256 "060dc160e897831b04c259940b36d985402b208cd653cc0d6ed4e08f03be88e0"
 
   depends_on "jq"
 
   def install
-    bin.install Dir["studio/studio"], Dir["bridge/bridge"], Dir["canvas/canvas"], Dir["catalog/catalog"], Dir["cdata/cdata"], Dir["commons/commons"], Dir["insthelp/insthelp"]
+    bin.install Dir["studio/studio"], Dir["canvas/canvas"], Dir["catalog/catalog"], Dir["cdata/cdata"], Dir["commons/commons"], Dir["insthelp/insthelp"]
   end
 
   def caveats; <<~EOS
-      Installed binaries are: studio, bridge, canvas, catalog, cdata, commons, and insthelp
+      Installed binaries are: studio, canvas, catalog, cdata, commons, and insthelp
       User pref file is stored in ~/.inst
   EOS
   end
 
   test do
     system "#{bin}/studio -v"
-    system "#{bin}/bridge -v"
     system "#{bin}/canvas -v"
     system "#{bin}/catalog -v"
     system "#{bin}/commons -v"
